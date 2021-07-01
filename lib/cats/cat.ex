f6c4e6889f32ecf69cddd879ef29e3d11b6970ae
Lise <lise.carpenter@gmail.com>
2021-07-01 10:55:30 +0700

    cat data structure

--- lib/cats/cat.ex
+++ lib/cats/cat.ex
@@ -1,11 +1,12 @@
 defmodule Cats.Cat do
-  defstruct [:id, :name, :age]
+  defstruct [:id, :name, :age, :breed]
 
-  def new(%{name: name, age: age}) do
+  def new(%{name: name, age: age, breed: breed}) do
     %__MODULE__{
       id: UUID.uuid4(),
       name: name,
-      age: age
+      age: age,
+      breed: breed
     }
   end
 end
