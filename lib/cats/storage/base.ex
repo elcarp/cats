5bb0b358d25e40c0c7b0b541ed595ddf0e270f81
Lise <lise.carpenter@gmail.com>
2021-07-12 13:52:44 +0700

    WIP

--- lib/cats/storage/base.ex
+++ lib/cats/storage/base.ex
@@ -10,20 +10,22 @@ defmodule Cats.Storage.Base do
       end
 
       def add(%unquote(module){} = resource) do
+        all()
+        checkIfExists(resource.id)
         # detect if id exists, if so return error
         Agent.update(__MODULE__, fn state -> [resource | state] end)
       end
 
       def checkIfExists(resource_id) do
-        Agent.get(__MODULE__, fn state ->
-          Enum.find(state, fn resource ->
+        #Agent.get(__MODULE__, fn state ->
+          Enum.find(all(), fn resource ->
             if resource.id == resource_id do
               IO.puts("cat exists")
             else
               IO.puts("no cat exists")
             end
           end)
-        end)
+        #end)
       end
 
       def all do
